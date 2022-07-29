1e3fe41b894d05f68fec39ff5973e02f68e9d1d1
LuosRemmus <87525791+LuosRemmus@users.noreply.github.com>
2022-07-29 12:26:45 +0300

    Added PostgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from requests import get
 import telebot
 from telebot import types
 from random import choice
 from TOKEN import token
+import psycopg2
+
+connection = psycopg2.connect(
+    host='localhost',
+    user='postgres',
+    password='zex.cmd5d68',
+    database='film_bot',
+    port='3228'
+)
 
 film_ids = {
     'Комедия': [8124, 6039, 426004, 1355139, 555, 21503, 464282, 2868, 455338],
@@ -10,44 +18,14 @@ film_ids = {
     'Боевик': [1318972, 41520, 389, 522, 9691, 3442, 257898, 471, 2717, 666],
     'Драма': [435, 329, 326, 448, 535341, 32898, 387556, 530, 4541, 81555]
 }
+already_seen = {
+    'Комедия': [],
+    'Ужасы': [],
+    'Боевик': [],
+    'Драма': []
+}
 
-
-def get_film(film_id: int) -> dict:
-    url = f"https://kinopoiskapiunofficial.tech/api/v2.2/films/{film_id}"
-    headers = \
-        {
-            'X-API-KEY': '0a562c7b-9703-4142-8b60-ea1b4dfbc8c2',
-            'Content-Type': 'application/json',
-        }
-
-    response = get(url=url, headers=headers).json()
-
-    film_name_ru = response['nameRu']
-    film_name_orig = response['nameOriginal']
-    film_img = response['posterUrl']
-    film_rating = response['ratingKinopoisk']
-    film_url = response['webUrl']
-    film_year = response['year']
-    film_length = f"{int(response['filmLength'] // 60)}ч. {int(response['filmLength'] % 60)}м. " \
-                  f"({response['filmLength']} минут)"
-    film_description = response['description']
-    film_countries = ', '.join([i['country'] for i in response['countries']])
-    film_genres = ', '.join([i['genre'] for i in response['genres']])
-
-    film = {
-        "film_name_ru": film_name_ru,
-        "film_name_orig": film_name_orig,
-        "film_img": film_img,
-        "film_rating": film_rating,
-        "film_url": film_url,
-        "film_year": film_year,
-        "film_length": film_length,
-        "film_description": film_description,
-        "film_countries": film_countries,
-        "film_genres": film_genres
-    }
-
-    return film
+connection.autocommit = True
 
 
 def telegram_bot(token):
@@ -75,17 +53,31 @@ def telegram_bot(token):
 
     @bot.message_handler(content_types=['text'])
     def genre(message):
-        print(message.text)
         if message.text in film_ids.keys():
-            film = get_film(choice(film_ids[message.text]))
-            bot.send_photo(message.chat.id, film['film_img'],
-                           f"<b>{film['film_name_ru']}</b> <i>({film['film_name_orig']})</i> <b>{film['film_year']}</b>\n\n"
-                           f"Рейтинг кинопоиска: {film['film_rating']}\n"
-                           f"Длительность фильме: {film['film_length']}\n"
-                           f"Страна: {film['film_countries']}\n"
-                           f"Жанр: {film['film_genres']}\n\n"
-                           f"Описание:\n<i>{film['film_description']}</i>\n\n"
-                           f"{film['film_url']}", parse_mode='html')
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    f"""SELECT films.* FROM films, genres 
+                        WHERE films.genre_id = genres.id AND genres.name = \'{message.text}\'"""
+                )
+
+                if len(film_ids[message.text]) != len(already_seen[message.text]):
+                    film = choice(cursor.fetchall())
+                    print(film)
+
+                    while film in already_seen[message.text]:
+                        film = choice(cursor.fetchall())
+
+                    bot.send_photo(message.chat.id, film[2],
+                                   f"<b>{film[0]}</b> <i>({film[1]})</i> <b>{film[5]}</b>\n\n"
+                                   f"Рейтинг кинопоиска: {film[3]}\n"
+                                   f"Длительность фильме: {film[6]} минут\n"
+                                   f"Страна: {film[8]}\n"
+                                   f"Жанр: {film[9]}\n\n"
+                                   f"Описание:\n<i>{film[7]}</i>\n\n"
+                                   f"{film[4]}", parse_mode='html')
+                    already_seen[message.text].append(film)
+                else:
+                    bot.send_message(message.chat.id, "Я уже порекомендовал все фильмы из этого жинра")
         else:
             bot.send_message(message.chat.id, "Я тебя не понимаю")
 
